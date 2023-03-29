25d79031b8f6c94657046e58bc687486365d9c7c
xuningda <1721417139@qq.com>
2023-03-29 11:18:42 +0800

    微信公众号授权登录2.5

--- src/main/java/com/example/springboot2demo/controller/api/WeChatAuthController.java
+++ src/main/java/com/example/springboot2demo/controller/api/WeChatAuthController.java
@@ -1,24 +1,11 @@
 package com.example.springboot2demo.controller.api;
 
-import cn.hutool.http.HttpUtil;
-import com.alibaba.fastjson.JSONObject;
-import com.example.springboot2demo.model.vo.WeChatConfigVO;
-import com.example.springboot2demo.model.vo.WeChatTemplateMsg;
-import com.example.springboot2demo.model.vo.WeChatVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author xnd
@@ -28,7 +15,7 @@ import java.util.Map;
 @Api(tags = "微信开放Api")
 @RestController
 @RequestMapping("")
-public class WeChatController {
+public class WeChatAuthController {
 
     @GetMapping("/MP_verify_DZcAbTjXNzdrQaVW.txt")
     @ApiOperation("auth")
